e236db8d6dbd72865bee74e4dfeb2a15f0e200f8
microjavapk <microjavapk@gmail.com>
2020-06-20 16:48:32 +0530

    testing with thenCallRealMethod

--- src/test/java/com/pradeep/service/UserServiceTest.java
+++ src/test/java/com/pradeep/service/UserServiceTest.java
@@ -6,12 +6,13 @@ import org.junit.Test;
 import org.powermock.api.mockito.PowerMockito;
 
 import com.pradeep.dao.UserDao;
+import com.pradeep.dao.UserDaoImpl;
 import com.pradeep.service.UserServiceImpl;
 
 public class UserServiceTest {
 @Test
 	public void testGetNameByUserId() {
-		UserDao mockDao=PowerMockito.mock(UserDao.class);
+		UserDaoImpl mockDao=PowerMockito.mock(UserDaoImpl.class);
 		PowerMockito.when(mockDao.findNameById(101)).thenReturn("Shikher");
 	
 		UserServiceImpl service =new UserServiceImpl(mockDao);
@@ -22,11 +23,11 @@ public class UserServiceTest {
 @Test
 public void testGetEmailUserId() {
 	
-	UserDao mockDao=PowerMockito.mock(UserDao.class);
-	PowerMockito.when(mockDao.findEmailById(101)).thenReturn("microjavapk@gmail.com");
+	UserDaoImpl mockDao=PowerMockito.mock(UserDaoImpl.class);
+	PowerMockito.when(mockDao.findEmailById(101)).thenCallRealMethod();
 	UserServiceImpl service=new UserServiceImpl(mockDao);
 	String email=service.getEmailByUserId(101);
 	System.out.println(email);
-	assertEquals("microjavapk@gmail.com", email);
+	assertEquals("pradeep.k@gmail.com", email);
 }
 }
